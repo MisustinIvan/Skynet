b96718fbb308662d3f41470b39280b0f5bc126f2
Ivan Misustin <ivanmis596@gmail.com>
2023-03-24 19:10:30 +0100

    made the code smaller on the screen

--- src/main.c
+++ src/main.c
@@ -50,111 +50,70 @@ void initializeNetwork(struct Network*nw) {
 
 void logNetworkFile(struct Network*nw) {
     FILE *file = fopen("./bin/network_info", "w");
-
     fprintf(file, "Network Info:\n");
-
     fprintf(file, "\n");
-
     fprintf(file, "Input Layer:\n");
     for (int i = 0; i < NUM_INPUTS; i++) {
         fprintf(file, "  Neuron index: %d\n", i);
         fprintf(file, "    bias: %f\n", nw->inputs_layer[i].bias);
         fprintf(file, "    output: %f\n", nw->inputs_layer[i].output);
-        //fprintf(file, "    input weights: \n");
-
-        // DONT YOU DARE UNCOMMENT THIS, IT WILL SEGFAULT BECAUSE THE WEIGHTS FOR THE INPUT LAYER ARE NOT ALLOCATED(its a NULL pointer)
-        //for (int j = 0; j < NUM_INPUTS; j++) {
-        //    fprintf(file, "i = %d, j = %d\n", i, j);
-        //    fprintf(file, "      input[%d]: %f\n", j, nw->inputs_layer[i].weights[j]);
-        //}
     }
-
     fprintf(file, "\n");
-
     fprintf(file, "Hidden Layer:\n");
     for (int i = 0; i < NUM_HIDDEN; i++) {
         fprintf(file, "  Neuron index: %d\n", i);
         fprintf(file, "    bias: %f\n", nw->hidden_layer[i].bias);
         fprintf(file, "    output: %f\n", nw->hidden_layer[i].output);
         fprintf(file, "    input weights: \n");
-
-        for (int j = 0; j < NUM_INPUTS; j++) {
-            fprintf(file, "      input[%d]:%f\n", j, nw->hidden_layer[i].weights[j]);
-        }
+        for (int j = 0; j < NUM_INPUTS; j++) { fprintf(file, "      input[%d]:%f\n", j, nw->hidden_layer[i].weights[j]); }
     }
-
     fprintf(file, "\n");
-
     fprintf(file, "Output Layer:\n");
     for (int i = 0; i < NUM_OUTPUT; i++) {
         fprintf(file, "  Neuron index: %d\n", i);
         fprintf(file, "    bias: %f\n", nw->output_layer[i].bias);
         fprintf(file, "    output: %f\n", nw->output_layer[i].output);
         fprintf(file, "    input weights: \n");
-
-        for (int j = 0; j < NUM_HIDDEN; j++) {
-            fprintf(file, "      input[%d]:%f\n", j, nw->output_layer[i].weights[j]);
-        }
+        for (int j = 0; j < NUM_HIDDEN; j++) { fprintf(file, "      input[%d]:%f\n", j, nw->output_layer[i].weights[j]); }
     }
     fclose(file);
 }
 
 void logNetworkStdin(struct Network*nw) {
     printf("Network Info:\n");
-
     printf("\n");
-
     printf("Input Layer:\n");
     for (int i = 0; i < NUM_INPUTS; i++) {
         printf("  Neuron index: %d\n", i);
         printf("    bias: %f\n", nw->inputs_layer[i].bias);
         printf("    output: %f\n", nw->inputs_layer[i].output);
-        //printf("    input weights: \n");
-
-        // DONT YOU DARE UNCOMMENT THIS, IT WILL SEGFAULT BECAUSE THE WEIGHTS FOR THE INPUT LAYER ARE NOT ALLOCATED(its a NULL pointer)
-        //for (int j = 0; j < NUM_INPUTS; j++) {
-        //    printf("i = %d, j = %d\n", i, j);
-        //    printf("      input[%d]: %f\n", j, nw->inputs_layer[i].weights[j]);
-        //}
     }
-
     printf("\n");
-
     printf("Hidden Layer:\n");
     for (int i = 0; i < NUM_HIDDEN; i++) {
         printf("  Neuron index: %d\n", i);
         printf("    bias: %f\n", nw->hidden_layer[i].bias);
         printf("    output: %f\n", nw->hidden_layer[i].output);
         printf("    input weights: \n");
-
-        for (int j = 0; j < NUM_INPUTS; j++) {
-            printf("      input[%d]:%f\n", j, nw->hidden_layer[i].weights[j]);
-        }
+        for (int j = 0; j < NUM_INPUTS; j++) { printf("      input[%d]:%f\n", j, nw->hidden_layer[i].weights[j]); }
     }
-
     printf("\n");
-
     printf("Output Layer:\n");
     for (int i = 0; i < NUM_OUTPUT; i++) {
         printf("  Neuron index: %d\n", i);
         printf("    bias: %f\n", nw->output_layer[i].bias);
         printf("    output: %f\n", nw->output_layer[i].output);
         printf("    input weights: \n");
-
-        for (int j = 0; j < NUM_HIDDEN; j++) {
-            printf("      input[%d]:%f\n", j, nw->output_layer[i].weights[j]);
-        }
-    
+        for (int j = 0; j < NUM_HIDDEN; j++) { printf("      input[%d]:%f\n", j, nw->output_layer[i].weights[j]); }
     }
 }
-
 
 int main(int argc, char *argv[]) {
 
     struct Network nw;
     initializeNetwork(&nw);
-    logNetworkStdin(&nw);
-    logNetworkFile(&nw);
+//    logNetworkStdin(&nw);
+//    logNetworkFile(&nw);
 
     return 0;
 }
